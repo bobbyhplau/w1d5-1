ce158d0d809ace1a9c6cf81d015641a36eb1266e
Govind Sandhu <gc.sandhu@gmail.com>
2019-05-31 19:12:02 +0000

    Function to export to sort array

---
+++ creating-exporting-modules/func.js
@@ -0,0 +1,15 @@
+var insert = function (array) {
+  return array;
+};
+
+module.exports.insert = insert;
+
+var numOrder = function (a,b) {
+  return -1
+}
+
+var sort = function (array) {
+  array.sort(numOrder)
+}
+
+module.exports.sort = sort;
